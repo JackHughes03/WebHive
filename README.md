b203b0c930e1d09ad439aafb0db7c7126bb0a981
JackHughes03 <jwhughes03@yahoo.com>
2023-11-25 16:04:12 +0000

    code push

---
+++ README.md
@@ -0,0 +1,5 @@
+## WebHive github respiratory
+This website is currently in development and has not been released yet.
+
+## Information
+This website was fully coded in [**VueJS**](https://vuejs.org/) and [**Tailwind**](https://tailwindcss.com/).
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': "#1e4bc8",
+      },
+    },
+  },
+  plugins: [],
+}
+
